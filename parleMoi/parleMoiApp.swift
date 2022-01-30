72561da2531a97a7f6fca4fe859727583cfe9182
Philippe MICHEL <philippe.michel1@gmail.com>
2022-01-30 15:04:53 +0100

    Initial Commit

---
+++ parleMoi/parleMoiApp.swift
@@ -0,0 +1,17 @@
+//
+//  parleMoiApp.swift
+//  parleMoi
+//
+//  Created by Philippe MICHEL on 30/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct parleMoiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
